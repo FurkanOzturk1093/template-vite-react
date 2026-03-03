2ba778780f507892dcf3deb22bad622dabb7e7fb
Furkan <furkanozturk1635@gmail.com>
2026-03-03 22:42:22 +0300

    Add server configuration for Vite with custom host

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// add vite-react.deploywise.dev 
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'vite-react.deploywise.dev'
+  }
+ 
 })
